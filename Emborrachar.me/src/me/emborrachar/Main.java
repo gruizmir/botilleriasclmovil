0e0de76c29987a08f8d09abc0197b796ff3254a8
gabriel <gabriel@britannia.(none)>
2013-01-11 16:57:48 -0300

    Agregada nueva forma de ventana a la info del marcador de Google Maps. Se incluyeron iconos para hielo, carbon y comida.

--- Emborrachar.me/src/me/emborrachar/Main.java
+++ Emborrachar.me/src/me/emborrachar/Main.java
@@ -1,10 +1,12 @@
 package me.emborrachar;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
-import android.widget.SlidingDrawer;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -20,36 +22,17 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class Main extends android.support.v4.app.FragmentActivity {
 	private GoogleMap mapa;
 	private int RADIO=2000;	//radio en metros de busqueda
-	private SlidingDrawer slide;
-	private TextView botName;
-	private TextView botAddress;
-	private TextView botHorario1;
-	private TextView botHorario2;
-	private ImageView botFood;
-	private ImageView botCoal;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		slide = (SlidingDrawer) findViewById(R.id.slidingDrawer1);
-		botName = (TextView) findViewById(R.id.bot_name);
-		botAddress = (TextView) findViewById(R.id.bot_address);
-		botHorario1 = (TextView) findViewById(R.id.bot_horario1);
-		botHorario2 = (TextView) findViewById(R.id.bot_horario2);
-		botHorario2 = (TextView) findViewById(R.id.bot_horario2);
-		botFood= (ImageView) findViewById(R.id.bot_food);
-		botCoal= (ImageView) findViewById(R.id.bot_coal);
-		
 		mapa = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
 		mapa.getUiSettings().setZoomControlsEnabled(false);
+		mapa.setInfoWindowAdapter(new BorrachoAdapter(this));
 		showStores();
 		mapa.setOnMarkerClickListener(new OnMarkerClickListener() {
 			public boolean onMarkerClick(Marker marker) {
-				botName.setText("San Pedro");
-				botAddress.setText("Agua santa 24");
-				botHorario1.setText("Lun a Jue: " + "11.00 a 01.00" + " hrs");
-				botHorario2.setText("Vie a Dom: " + "11.00 a 03.00" + " hrs");
-				slide.animateToggle();
 				return false;
 			}
 		});
@@ -60,13 +43,14 @@ public class Main extends android.support.v4.app.FragmentActivity {
 	private void showStores(){
 		mapa.addMarker(new MarkerOptions()
 		.position(new LatLng(-33.039983,-71.592681))
-		.title("Cachorritos House")
-		.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
+		.title("San Pedro")
+		.snippet("Agua santa 24\n" +  "Lun a Jue: " + "11.00 a 01.00" + " hrs\n" +
+				"Vie a Dom: " + "11.00 a 03.00" + " hrs\nYES\nYES\nNO")
+				.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
@@ -83,6 +67,42 @@ public class Main extends android.support.v4.app.FragmentActivity {
 			break;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	class BorrachoAdapter implements GoogleMap.InfoWindowAdapter{
+		private LayoutInflater inf;
+		public BorrachoAdapter(Context cont){
+			inf = LayoutInflater.from(cont);
+		}
+
+		@Override
+		public View getInfoContents(Marker marker) {
+			return null;
+		}
+
+		@Override
+		public View getInfoWindow(Marker marker) {
+			View mView = inf.inflate(R.layout.balloon, null); 
+			TextView botName = (TextView) mView.findViewById(R.id.bot_name);
+			TextView botAddress = (TextView) mView.findViewById(R.id.bot_address);
+			TextView botHorario1 = (TextView) mView.findViewById(R.id.bot_horario1);
+			TextView botHorario2 = (TextView) mView.findViewById(R.id.bot_horario2);
+			ImageView botFood= (ImageView) mView.findViewById(R.id.bot_food);
+			ImageView botCoal= (ImageView) mView.findViewById(R.id.bot_coal);
+			botName.setText(marker.getTitle());
+			String[] values = marker.getSnippet().split("\n");
+			botAddress.setText(values[0]);
+			botHorario1.setText(values[1]);
+			botHorario2.setText(values[2]);
+			if(values[3].equals("YES")){
+				botFood.setImageResource(R.drawable.food);
+			}
+			if(values[4].equals("YES")){
+				botCoal.setImageResource(R.drawable.coal);
+			}
+			return mView;
+		}
+
 	}
 
 }
